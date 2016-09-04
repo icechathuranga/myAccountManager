dc57190b2f4faaaee89f2f53098066572b61d9a1
Nuwan <nuwanchathuranga.t@gmail.com>
2016-09-04 23:10:23 +0530

    delete operation

--- src/APIBundle/Controller/UsersController.php
+++ src/APIBundle/Controller/UsersController.php
@@ -26,6 +26,10 @@ use FOS\RestBundle\Controller\Annotations\View;
 use APIBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use FOS\RestBundle\Controller\FOSRestController;
+use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Doctrine\Common\Persistence\ObjectManager;
 
 /**
  * [UsersController]
@@ -37,7 +41,15 @@ use FOS\RestBundle\Controller\FOSRestController;
 class UsersController extends FOSRestController {
 
     /**
-     *
+     * @api
+     * @ApiDoc(
+     *  resource = true,
+     *  description = "Users related CRUD operations",
+     *  input = "",
+     *  views={},
+     *  requirements = {
+     *  })
+     * 
      * @return array
      * @View()
      */
@@ -52,8 +64,60 @@ class UsersController extends FOSRestController {
      * @return array
      * @View()
      * @ParamConverter("user", class="APIBundle:User")
+     *
+     * @api
+     * @ApiDoc(
+     *  resource = true,
+     *  description = "User related CRUD operations",
+     *  input = "APIBundle\Entity\User",
+     *  views={},
+     *  requirements = {
+     *      {
+     *          "name" = "$user",
+     *          "dataType" = "integer",
+     *          "requirement" = "\w",
+     *          "description" = "Base on the user id this method will return a user object"
+     *      }
+     *  })
+     * 
      */
     public function getUserAction(User $user) {
+        return array('user' => $user);
+    }
+
+    /**
+     * @return array
+     * @View()
+     * @param User $user
+     * @ParamConverter("user", class="APIBundle:User")
+     */
+    public function patchUserAction(User $user) {
+        return array('user' => $user);
+    }
+
+    /**
+     * @return array
+     * @View()
+     * @param User $user
+     * @ParamConverter("user", class="APIBundle:User")
+     */
+    public function deleteUserAction(User $user) {
+        $id = $user->getId();
+        $this->getDoctrine()->getManager()->remove($user);
+        $this->getDoctrine()->getManager()->flush();
+        return array('deleted' => $id);
+    }
+
+    /**
+     * @return array
+     * @View()     
+     * 
+     */
+    public function postUserAction() {
+//        echo $this->getParameter('username');
+//        die('herer');
+        $this->getDoctrine()->getManager()->persist($user);
+        $this->getDoctrine()->getManager()->flush();
         return array('user' => $user);
     }
 
